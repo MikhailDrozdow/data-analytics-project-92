9da1335b450d57c95ef224045099a26f116fd2b0
Mikhail Drozdow <57599312+MikhailDrozdow@users.noreply.github.com>
2024-03-13 21:15:36 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -50,7 +50,7 @@ order by
 	average_income;
 	
 
-/*Данный скрипт дает информацию о выручке по дням недели*/
+/*Данный скрипт дает информацию о выручке по продавцам и по дням недели*/
 
 select 
 	name,
@@ -77,3 +77,78 @@ from
 		num_day_of_week,
 		name) as t1
 ;
+
+
+/*Данный скрипт вычисляет кол-во покупателей по категшориям возраста*/
+
+select
+	case 
+		when age between 16 and 25 then '16-25'
+		when age between 26 and 40 then '26-40'
+		else '40+'
+	end age_category,
+	customer_id, 
+	age
+from
+	customers;
+
+
+/*Данный скрипт дает данные по количеству уникальных покупателей и выручке, которую они принесли*/
+
+select 
+	to_char(s.sale_date, 'YYYY-MM') as date,
+	count(distinct s.customer_id) total_customers,
+	floor(sum(s.quantity * p.price)) income
+from sales s
+left join products p 
+	on p.product_id = s.product_id 
+group by 
+	to_char(s.sale_date, 'YYYY-MM')
+order by 
+	date;
+
+
+/*Данный скрипт дает инфо о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0)*/
+
+select
+	customer,
+	sale_date,
+	seller
+from
+	(select
+	    s.sales_id ,
+	    c.first_name || ' ' || c.last_name customer,
+	    s.sale_date sale_date,
+	    e.first_name || ' ' || e.last_name seller,
+	    p.price,
+	    s.customer_id,
+	    s.product_id,
+	    rank() over(partition by s.customer_id order by s.sale_date) rnk
+	from
+	    sales s 
+	left join customers c 
+	    on c.customer_id = s.customer_id 
+	left join employees e 
+	    on e.employee_id = s.sales_person_id
+	left join products p 
+	    on p.product_id = s.product_id 
+	order by 
+	    customer_id, customer, sale_date) as t1
+where
+	rnk = 1 and price = 0
+group by 
+	customer,
+	sale_date,
+	seller
+;
+
+
+
+
+
+
+
+
+
+
+
